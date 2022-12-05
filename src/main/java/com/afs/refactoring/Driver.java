e1b457de0fc9c7b9b0fa91e36ce4a87a0cd2c336
Wongje4 <>
2022-12-05 16:07:41 +0800

    refactor: rename a and d to age and driver

--- src/main/java/com/afs/refactoring/Driver.java
+++ src/main/java/com/afs/refactoring/Driver.java
@@ -3,8 +3,8 @@ package com.afs.refactoring;
 public class Driver {
     public int age;
 
-    public Driver(int a) {
-        this.age = a;
+    public Driver(int age) {
+        this.age = age;
     }
 }
 
